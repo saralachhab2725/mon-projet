a0d6d5d709f1dba16b53f31b98c1e447c290c2a0
saralachhab2725 <saralachhab2725@gmail.com>
2025-12-19 09:46:56 +0100

    Refactor index.php for improved structure and readability

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 require_once "pdo.php";
+require_once "util.php";
 session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Lachhab Sara - Resume Registry</title>
-<a href="login.php">Please log in</a>
+<head>
+    <title>Lachhab Sara - Resume Registry</title>
 </head>
 <body>
 <h1>Profiles</h1>
-<?php require_once "util.php"; flashMessage(); ?>
 
 <?php
+flashMessage();
+
 $stmt = $pdo->query("SELECT profile_id, first_name, last_name, headline FROM Profile");
 echo('<table border="1">');
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "<tr><td>";
-    echo '<a href="view.php?profile_id='.$row['profile_id'].'">'.htmlentities($row['first_name']).' '.htmlentities($row['last_name']).'</a>';
+    echo '<a href="view.php?profile_id='.htmlentities($row['profile_id']).'">'
+         .htmlentities($row['first_name']).' '.htmlentities($row['last_name']).'</a>';
     echo "</td><td>".htmlentities($row['headline'])."</td>";
 
     if (isset($_SESSION['user_id'])) {
@@ -35,14 +38,8 @@ if (!isset($_SESSION['name'])) {
     echo '<p><a href="add.php">Add New Entry</a> | <a href="logout.php">Logout</a></p>';
 }
 ?>
-<?php
-session_start();
-
-if ( ! isset($_SESSION['name']) ) {
-    echo '<a href="login.php">Please log in</a>';
-}
-?>
-
 </body>
 </html>
+
+
 
